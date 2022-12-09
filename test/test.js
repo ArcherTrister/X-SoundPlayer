7e87dd4be7f3ba61db53cab6b463450ce82ebe8c
ArcherTrister <archertrister@outlook.com>
2022-12-09 12:32:12 +0800

    update test

--- test/test.js
+++ test/test.js
@@ -5,20 +5,22 @@ const soundPlayer = require("../dist/index").soundPlayer;
 
 describe("soundPlayer test", () => {
   it("play", async () => {
-    console.log(soundPlayer);
     const options = {
       soundPath: path.join(__dirname, "./audios/notify.wav"),
       volume: 2,
-      time: 3,
+      time: 2,
     };
+    // console.time();
     const result = await soundPlayer
       .play(options)
       .then(() => {
         return true;
       })
-      .catch(() => {
+      .catch((e) => {
+        console.log(e);
         return false;
       });
     expect(result).to.equal(true);
+    // console.timeEnd();
   }).timeout(200000);
 });
