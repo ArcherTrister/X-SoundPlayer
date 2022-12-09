cb06b5dae6985ad767fb271b92d45b6ed0dd4219
ArcherTrister <archertrister@outlook.com>
2022-12-09 15:54:30 +0800

    update config

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,55 @@
 import { exec } from "child_process";
-import { SoundPlayer, PlayOptions, Player } from "./types";
 
-export type { SoundPlayer, PlayOptions, Player } from "./types";
+/**
+ * 播放器
+ */
+export enum Player {
+  afplay = 0,
+  aplay,
+  ffplay,
+  mpg123,
+  mpg321,
+}
+
+/**
+ * 播放配置
+ * @property soundPath {string} 音频文件路径
+ * @property player {Player} 播放器
+ * @property time {number?} 播放时长
+ * @property volume {number} 播放音量
+ */
+export interface PlayOptions {
+  /**
+   * 音频文件路径
+   * @type {string}
+   */
+  soundPath: string;
+  /**
+   * 播放器
+   * @type {Player}
+   */
+  player?: Player;
+  /**
+   * 播放时长
+   * @type {number?}
+   */
+  time?: number | undefined;
+  /**
+   * 播放音量
+   * @type {number}
+   */
+  volume: number;
+}
+
+export interface SoundPlayer {
+  /**
+   * 播放
+   * @typedef { import('./xxx').PlayOptions } PlayOptions
+   * @param options 播放配置
+   * @returns
+   */
+  play: (options: PlayOptions) => Promise<void>;
+}
 
 /**
  * MAC PLAY COMMAND
